ee3a8ce159c5d904c5d44a8c12bd359d03a3f794
Diego Pertierra <dpertierra@hotmail.com>
2020-11-17 11:50:27 -0300

    Adding script to fix Media Foundation DLL issue

---
+++ fixmf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo ********************************************************
+echo Fix Media Foundation DLL issue
+echo ********************************************************
+read -p 'Enter the full path to your steamapps folder inside your steam library folder' steamapps
+read -p 'Enter the app id of the game you want to fix the Media Foundation DLL' appid
+export WINEARCH=win64
+export WINEPREFIX="$steamapps/compatdata/$appid/pfx"
+export PROTON="$steamapps/common/Proton 5.0/"
+cd ~/Downloads
+git clone https://github.com/z0z0z/mf-install
+cd mf-install
+WINEPREFIX="$steamapps/compatdata/$appid/pfx" ./mf-install.sh --proton
+cd ..
+git clone https://github.com/z0z0z/mf-installcab
+cd mf-installcab
+WINEPREFIX="$steamapps/compatdata/$appid/pfx" ./install-mf-64.sh
