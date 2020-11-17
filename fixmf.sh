fb0a8840202aaafd671774dafde16d9d0a0e37fe
Diego Pertierra <dpertierra@hotmail.com>
2020-11-17 12:04:23 -0300

    Correcting echos

--- fixmf.sh
+++ fixmf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "********************************************************************************"
-echo "                        Fix Media Foundation DLL issue                          "
-echo "********************************************************************************"
+echo "**********************************************************************************"
+echo "                        Fix Media Foundation DLL issue                            "
+echo "**********************************************************************************"
 read -p 'Enter the full path to your steamapps folder inside your steam library folder: ' steamapps
 read -p 'Enter the app id of the game you want to fix the Media Foundation DLL: ' appid
 export WINEARCH=win64
@@ -18,3 +18,5 @@ WINEPREFIX="$steamapps/compatdata/$appid/pfx" ./install-mf-64.sh
 
 rm -r ~/Downloads/mf-install
 rm -r ~/Downloads/mf-installcab
+
+echo "All done now your game should be working fine"
